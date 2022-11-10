3cf93daa870cebffa5aff13d26c1a7237df9c17e
User1284067 <116982061+User1284067@users.noreply.github.com>
2022-11-09 20:21:37 -0600

    Update Exercise 4.py

--- Assignment 2/Exercise 4.py
+++ Assignment 2/Exercise 4.py
@@ -2,10 +2,52 @@ import numpy as np
 import scipy as sp
 import random as rd
 import matplotlib.pyplot as plt
+import os as os
+import re as re
+
+print(os.getcwd())
+sunspots_lst= open("sunspots.txt", "r").readlines()
 
 
-sunspots=open("sunspots.txt","r")
+month=[]
+sunspots=[]
 
 
+#intial code before modification
+'''
+for i in range(len(sunspots_lst)):
+    split=re.split(r"\t|\n",sunspots_lst[i],2)
+    m,s,*_=split
+    month.append(float(m))
+    sunspots.append(float(s)) 
+'''
 
+    
+    
+for i in range(1000):
+    split=re.split(r"\t|\n",sunspots_lst[i],2)
+    m,s,*_=split
+    month.append(float(m))
+    sunspots.append(float(s)) 
+    
+    
 print(sunspots)
+mean_sunspots=[]
+for i in range(1000):
+    j=0
+    y=0
+    while j<=i:
+        y=0.1*(y+j)
+        j=j+1
+    mean_sunspots.append(y) #I dont know why this didnt work but oh well
+
+
+plt.plot(month,sunspots)
+plt.plot(month,mean_sunspots)
+plt.title("sunspots vs time")
+plt.xlabel("time (months)")
+plt.ylabel("sunspots")
+plt.show()
+
+
+
